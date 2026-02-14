962ac632899fd876105d45a2b24bcfe441e52578
zjsan <zjsantos25@gmail.com>
2026-02-14 20:51:27 +0800

    morphing the letter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 
 export default function App() {
   const [isOpen, setIsOpen] = useState(false);
